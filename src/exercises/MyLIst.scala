585b46e82d750d4a142efc338bfbba6208b91f2d
Dzikibilly <dzikibilly@gmail.com>
2020-01-25 19:19:47 +0100

    My List from laptop

---
+++ src/exercises/MyLIst.scala
@@ -0,0 +1,102 @@
+package exercises
+
+import scala.runtime.Nothing$
+
+
+abstract class MyList[+A] {
+
+  def head: A
+  def tail: MyList[A]
+  def isEmpty: Boolean
+  def add[B >: A](element: B): MyList[B]
+  def printElements: String
+  // polymorphic call
+  override def toString: String = "[" + printElements + "]"
+
+  def map[B](transform: MyTransformer[A,B]): MyList[B]
+  def flatmap[B](mLis: MyTransformer[A,MyList[B]]): MyList[B]
+  def filter(pred: MyPredicat[A]): MyList[A]
+  def ++[B >:A](list: MyList[B]): MyList[B]
+
+}
+
+case object Empty extends MyList[Nothing] {
+  def head: Nothing = throw new NoSuchElementException
+  def tail: MyList[Nothing] = throw new NoSuchElementException
+  def isEmpty: Boolean = true
+  def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
+  def printElements: String = ""
+
+  def map[B](transform: MyTransformer[Nothing,B]): MyList[B] = Empty
+  def filter(pred: MyPredicat[Nothing]): MyList[Nothing] = Empty
+
+  def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
+  def flatmap[B](mLis: MyTransformer[Nothing,MyList[B]]): MyList[B] = Empty
+}
+
+case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
+  def head: A = h
+  def tail: MyList[A] = t
+  def isEmpty: Boolean = false
+  def add[B >: A](element: B): MyList[B] = new Cons(element, this)
+  def printElements: String =
+    if(t.isEmpty) "" + h
+    else h + " " + t.printElements
+
+  def filter(predicate: MyPredicat[A]): MyList[A] = {
+    if (predicate.test(h)) new Cons[A](h, t.filter(predicate))
+    else t.filter(predicate)
+  }
+  def map[B](transform: MyTransformer[A,B]): MyList[B] = {
+    new Cons[B](transform.transform(h), t.map(transform))
+  }
+
+  def ++[B >: A](list: MyList[B]): MyList[B] = new Cons[B](h, tail ++ list)
+
+  def flatmap[B](mLis: MyTransformer[A,MyList[B]]): MyList[B] = {
+    mLis.transform(h) ++ t.flatmap(mLis)
+  }
+}
+
+trait MyPredicat[-T]{
+  def test(test: T): Boolean
+}
+
+trait MyTransformer[-A, B] {
+  def transform(obj: A): B
+}
+
+
+object MyList extends App {
+  val listOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  val cloneListOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  val anotherListOfIntegers: MyList[Int] = new Cons(4, new Cons(5, Empty))
+  val anotherListOfIntegers_2: MyList[Int] = new Cons(4, new Cons(5, new Cons[Int](56, Empty)))
+  val listOfStrings: MyList[String] = new Cons("Hello", new Cons("Scala", Empty))
+
+  println(listOfIntegers.toString)
+  println(listOfStrings.toString)
+  println(anotherListOfIntegers.toString)
+
+  val mapowane = anotherListOfIntegers.map(new MyTransformer[Int,Int] {
+    override def transform(obj: Int): Int = obj * 2
+  })
+
+  val predecate = new MyPredicat[Int] {
+    override def test(test: Int): Boolean = test % 2 ==0
+  }
+
+  println(anotherListOfIntegers.filter(new MyPredicat[Int] {
+    override def test(test: Int): Boolean = test % 2 ==0
+  }).toString)
+
+  println(mapowane.filter(predecate).toString)
+
+  val sum = anotherListOfIntegers ++ mapowane
+
+  println(sum.toString)
+
+  val forFlatMap = anotherListOfIntegers_2.flatmap(new MyTransformer[Int, MyList[Int]] {
+    override def transform(obj: Int): MyList[Int] = new Cons[Int](obj, new Cons[Int](obj + 1, Empty))
+  })
+}
--- src/playground/inheritance_exercise.scala
+++ src/playground/inheritance_exercise.scala
@@ -1,61 +1,92 @@
-package playground
-
-import scala.collection.View.Empty
-
-object inheritance_exercise extends App {
-
-  abstract class MyLIst[+A] {
-    def head: A
-
-    def tail: MyLIst[A]
-
-    def isEmpty: Boolean
-
-    def add[B >: A](n: B): MyLIst[B]
-
-    def printToConsole: String
-
-    override def toString: String = "[" + printToConsole + "]"
-  }
-
-  class Empty extends MyLIst[Nothing] {
-    def head: Nothing = throw new NoSuchElementException
-
-    def tail: MyLIst[Nothing] = throw new NoSuchElementException
-
-    def isEmpty: Boolean = true
-
-    def add[B >: Nothing](n: B): Const[B] = new Const(n, this)
-
-    def printToConsole: String = ""
-  }
-
-  class Const[+A](pierwszy: A, rest: MyLIst[A]) extends MyLIst[A]{
-      def head: A = pierwszy
-
-      def tail: MyLIst[A] = rest
-
-      def isEmpty: Boolean = false
-
-      def add[B >:A](n: B): Const[B] = new Const(n, this)
-
-       def printToConsole: String = {
-        if(rest.isEmpty) "" + head
-        else
-          head + " " + rest.printToConsole
-      }
-  }
-
-
-  val lista_1 = new Empty
-  val list_2 = new Const[String]("lista_1", new Const[String]("abcd",
-    lista_1))
-  println(list_2.toString)
+//package playground
 //
-//  val lista_2 = new Const(1, lista_1)
+//import scala.collection.View.Empty
+//import scala.runtime.Nothing$
 //
-//  val lista_3 = new Const(123, lista_2)
+//object inheritance_exercise extends App {
 //
-//  println(lista_2.toString)
-//  println(lista_3.toString)
-}
+//  abstract class MyLIst[+A] {
+//    def head: A
+//
+//    def tail: MyLIst[A]
+//
+//    def isEmpty: Boolean
+//
+//    def add[B >: A](n: B): MyLIst[B]
+//
+//    def printToConsole: String
+//
+//    override def toString: String = "[" + printToConsole + "]"
+//
+//    def map[B](transformer: MyTransformer[A,B]): MyLIst[B]
+//    def flatMap[B](transformer: MyTransformer[A, MyLIst[B]]): MyLIst[B]
+//    def filter(predicate: MyPredicate[A]): MyLIst[A]
+//
+//  }
+//
+//  class Empty extends MyLIst[Nothing] {
+//    def head: Nothing = throw new NoSuchElementException
+//
+//    def tail: MyLIst[Nothing] = throw new NoSuchElementException
+//
+//    def isEmpty: Boolean = true
+//
+//    def add[B >: Nothing](n: B): Const[B] = new Const(n, this)
+//
+//    def printToConsole: String = ""
+//
+//    def map[B](transform: MyTransformer[Nothing,B]): MyLIst[B] = Empty
+//    def flatMap[B](transformer: MyTransformer[Nothing, MyLIst[B]]): MyLIst[B] = Empty
+//    def filter(predicate: MyPredicate[Nothing]): MyLIst[Nothing] = Empty
+//
+//  }
+//
+//  class Const[+A](pierwszy: A, rest: MyLIst[A]) extends MyLIst[A]{
+//      def head: A = pierwszy
+//
+//      def tail: MyLIst[A] = rest
+//
+//      def isEmpty: Boolean = false
+//
+//      def add[B >:A](n: B): Const[B] = new Const(n, this)
+//
+//       def printToConsole: String = {
+//        if(rest.isEmpty) "" + head
+//        else
+//          head + " " + rest.printToConsole
+//      }
+//
+//    def map[B](transformer: MyTransformer[A,B]): MyLIst[B] = {
+//       new Const(transformer.transform(pierwszy), rest.map(transformer))
+//    }
+//    def flatMap[B](transformer: MyTransformer[A, MyLIst[B]]): MyLIst[B] = Empty
+//    def filter(predicate: MyPredicate[A]): MyLIst[A] = {
+//      if(predicate.test(pierwszy)) new Const[A](pierwszy, rest.filter(predicate))
+//      else rest.filter(predicate)
+//    }
+//  }
+//
+//  trait MyPredicate[-T]{
+//    def test(elem: T): Boolean
+//  }
+//
+//  trait MyTransformer[-A,B]{
+//    def transform(elem: A): B
+//  }
+//
+//
+//  val lista_1 = new Empty
+//  val list_2 = new Const[String]("lista_1", new Const[String]("abcd",
+//    lista_1))
+//  println(list_2.toString)
+//
+//
+//
+////
+////  val lista_2 = new Const(1, lista_1)
+////
+////  val lista_3 = new Const(123, lista_2)
+////
+////  println(lista_2.toString)
+////  println(lista_3.toString)
+//}
